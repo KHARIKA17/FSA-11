6d48505dfdd6fc4ddd8bf857c7973bcc3364b0ba
KHARIKA17 <60010885+KHARIKA17@users.noreply.github.com>
2021-03-04 09:20:17 -0600

    modified files

---
+++ location.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        Name: 'Villago',
+        type:'triangle',
+        coordinates: 
+        [
+          {
+        latitude: 33.4271871,
+        longitude: -111.8848516
+        },
+       {
+      latitude: 33.4271852,
+      longitude: -111.8848570
+      },
+      {
+     latitude: 33.4271822,
+     longitude: -111.8848589
+     }
+ ]
+ }
+ ];
