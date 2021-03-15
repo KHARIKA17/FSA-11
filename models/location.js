064a9005fada458799a14092b7bd1c5ca4fb63c0
GUNDUPOOJA <gundupooja1995@gmail.com>
2021-03-14 23:13:45 -0500

    added location attributes in models/location.js

--- models/location.js
+++ models/location.js
@@ -2,29 +2,44 @@
  *  Location model
  *  Describes the characteristics of each attribute in a Location resource.
  *
- * @author Denise Case <dcase@nwmissouri.edu>
- *
- * For more information about defining sequelize models, see
- * https://sequelize.org/v5/manual/data-types.html
- * https://sequelize.org/master/manual/validations-and-constraints.html
- *
- * For validators see: https://github.com/validatorjs/validator.js
+ * @author gundupooja
  *
  */
-// Export a function that defines the model.
-// It automatically receives the Sequelize connection parameter.
 
 module.exports = (db, DataTypes) => {
   db.define(
     'Location',
     {
-      // sqlite creates a rowid attribute automatically
-      name: {
-        type: DataTypes.STRING(50),
+      locationId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false
       },
-    },
-    {
-      // Other model options go here
-    }
-  );
-};
+      locationName: {
+        type: DataTypes.STRING(30),
+        unique: true,
+        required: true,
+        allowNull: false
+      },
+      locationLatitude: {
+        type: DataTypes.DECIMAL(10, 8),
+        allowNull: false,
+        validate: { min: -90, max: 90 }
+      },
+      locationLongitude: {
+        type: DataTypes.DECIMAL(11, 8),
+        allowNull: false,
+        validate: { min: -180, max: 180 }
+      },
+      locationValue: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      radius: {
+        type: DataTypes.INTEGER,
+        required: true,
+      }
+    });
+  
+  };
+ 
